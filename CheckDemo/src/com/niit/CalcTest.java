dfe9409cb94644c60e839be7e5cbbd838bca09ac
Training <Training@192.168.252.147>
2018-05-04 12:35:17 +0530

    version 1.0

--- CheckDemo/src/com/niit/CalcTest.java
+++ CheckDemo/src/com/niit/CalcTest.java
@@ -1,6 +1,28 @@
 package com.niit;
 
 public class CalcTest {
- 
+ Integer a,b;
+
+public Integer getA() {
+	return a;
+}
+
+public void setA(Integer a) {
+	this.a = a;
+}
+
+public Integer getB() {
+	return b;
+}
+
+public void setB(Integer b) {
+	this.b = b;
+}
+
+public CalcTest(Integer a, Integer b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
 	
 }
