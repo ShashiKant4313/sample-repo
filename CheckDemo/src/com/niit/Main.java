52367b076d46959513d4ba7bdea66e5ca0ddbf65
Shashi Kant <shashikant4313@gmail.com>
2020-05-24 03:29:44 +0530

    changes in comment

--- CheckDemo/src/com/niit/Main.java
+++ CheckDemo/src/com/niit/Main.java
@@ -4,13 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		// values hardcoded
 		int a = 5;
 		int b = 10;
 		
 		// new object made for value accessing
 		CalcTest calc = new CalcTest(a, b);
 		
-		//display output a
+		//display output a and b
 		System.out.println("Get value of A : " + calc.getA());
 		System.out.println("Get value of B : " + calc.getB());
 		
